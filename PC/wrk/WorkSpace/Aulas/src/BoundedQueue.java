7b6d6abf03e27bd6ee6a253a639631c8e5590bab
Gambit <ricardo.marta@gmail.com>
2011-11-16 20:30:50 +0000

    BoundedQueue.java

--- PC/wrk/WorkSpace/Aulas/src/BoundedQueue.java
+++ PC/wrk/WorkSpace/Aulas/src/BoundedQueue.java
@@ -4,31 +4,85 @@ public class BoundedQueue<T> {
 	
 	private final LinkedList<T> _buffer;
 	private final LinkedList<TakeRequest<T>> _requests;
+	private final int _capacity;
 	
 	private class TakeRequest<T>
 	{
 		public T _element;	
 	}
 	
-	public BoundedQueue()
+	public BoundedQueue(int capacity)
 	{
-		_buffer = new LinkedList<TakeRequest<T>>();
-		
+		_buffer = new LinkedList<T>();
+		_requests = new LinkedList<TakeRequest<T>>();
+		_capacity = capacity;
 	}
 	
 	
 	public void put (T elem) throws InterruptedException
 	{
-		
-		
-		
+		synchronized (_buffer) {
+			if(_buffer.size() != _capacity)
+			{
+				if (_requests.isEmpty())
+				{
+					_buffer.addLast(elem);
+					return;
+				}
+				TakeRequest<T> tr = _requests.removeFirst();
+				tr._element = elem;
+				_buffer.notifyAll();
+				return;
+			}
+			TakeRequest<T> tr = new TakeRequest<T>();
+			tr._element = elem;
+			_requests.addLast(tr);
+			try{
+				while (true)
+				{
+					_buffer.wait();
+					if(tr._element == null) return;
+				}	
+			}
+			catch (InterruptedException ie){
+				
+			}
+		}		
 	}
 	
 	
 	
 	public T take() throws InterruptedException
 	{
-		
+		synchronized (_buffer) {
+			if(_buffer.size() != 0)
+			{
+				T elem = _buffer.removeFirst();
+				if (!_requests.isEmpty())
+				{
+					TakeRequest<T> tr = _requests.removeFirst();
+					_buffer.addLast(tr._element);
+					tr._element = null;
+					_buffer.notifyAll();
+				}
+				return elem;
+			}
+			
+			TakeRequest<T> tr = new TakeRequest<T>();
+			_requests.addLast(tr);
+			try{
+				while (true)
+				{
+					_buffer.wait();
+					if(tr._element != null) return tr._element;
+				}	
+			}
+			catch (InterruptedException ie){
+				
+			}
+			
+			
+		}	
 		
 		
 	}
