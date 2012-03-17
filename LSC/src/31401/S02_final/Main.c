e1be3cbcdc0dbfb08dc67015c4fdc821f2834043
Nuno Cancelo <nuno.cancelo@gmail.com>
2012-03-17 18:37:46 +0000

    Timer works more or less fine

--- LSC/src/31401/S02_final/Main.c
+++ LSC/src/31401/S02_final/Main.c
@@ -24,74 +24,23 @@
 #include "Types.h"
 
 char * const image_ptr = (char*) 0x600000;
-//iNODE * const images = (iNODE *) 0x400000;
-
-//#define IMAGES_HOME  "image"
-//static PARTITION part;
-//static int total;
-
-//static PartitionTable table;
-//static SuperBlock superblock;
-//static INode	inodeBuffer[INODE_BUFFER];
-
-/*
-void lsc_startup()
-{	
-   SimpleDir directory;
-   char buffer[BUFFER_SIZE];
-	readPartition(&part, 1);
-   
-	total = getDirectoryContentLength(&part, IMAGES_HOME,buffer,&directory);
-	getDirectoryContent(&part, IMAGES_HOME, images,&directory);	
-}
-
-void lsc_run()
-{
-   int idx = 0;
-   for(idx = 0 ; idx + 1 ;idx = (idx + 1) % total){
-      readFile(&part, &images[idx], (void*) image_ptr);
-      DisplayBMPImage(image_ptr);
-      Timer_delay(1000);
-   }
-}
-*/
 void lsc_main() {
+   Timer_start();
 	PartitionTable table;
 	SuperBlock superblock;
 	INode	inodeBuffer[INODE_BUFFER];
+   //U8 image_ptr[1440000];
 	U32 imageNbr=0, idx=0, status;
 	Minix_Start(&table,&superblock);
-		if (superblock.s_firstdatazone == 903){
-			RGBPixel green = {31,31,31};
-			DisplayColor(green);
-			Timer_delay(1000);
-		}
-	//Obtem os inodes das Imagens
 	imageNbr = Minix_LoadImages(&(table.Table),&superblock,inodeBuffer);
-	RGBPixel green = {0,16,0};
-	RGBPixel blue = {16,0,0};
-	RGBPixel red = {0,0,16};
-	
 	while(1){
-		if (imageNbr == 255){
-			DisplayColor(green);
-		}else{
-			DisplayColor(red);
-		}
-		Timer_delay(1000);
-
-		/*
 		for(idx=0;idx<imageNbr;++idx)
 		{
 			status = Minix_ReadFileDataZone(&(table.Table),&(inodeBuffer[idx]),image_ptr);
-			if (status)
-			{
-				DisplayColor(blue);
-				Timer_delay(1000);
-			}
 			DisplayBMPImage(image_ptr);
+			Timer_delay(5000);
 		}
-		*/
+		
 	}
 }
 
--- LSC/src/31401/S02_final/dev/timer/timer.c
+++ LSC/src/31401/S02_final/dev/timer/timer.c
@@ -25,26 +25,33 @@
 #define MILI		1000
 #define SECOND		1
 #define DEFAULT_MS	10
+#define MAX_TIMER_COUNT 65536
 
 void Timer_start()
 {
-	int timerSetup = (FREQUENCY/MILI)*DEFAULT_MS;
+	int timerSetup = (FREQUENCY/MILI)*DEFAULT_MS + 1 ;
 	outb(ACTION,__CONTROL__);
 	outb( timerSetup         & LOW_BYTE_MASK,  __SELECT_COUNTER_0__);        
 	outb((timerSetup >> BYTE)& LOW_BYTE_MASK,  __SELECT_COUNTER_0__);  
 }
 
-static S32 Timer_elapsed(int last_read)
+static U32 Timer_Read()
 {
-	outb(0,__CONTROL__);                   
+ 	outb(0,__CONTROL__);                   
 	int timer_read  = inb(__SELECT_COUNTER_0__); 
 	    timer_read += inb(__SELECT_COUNTER_0__)<< BYTE;
-	return timer_read -  last_read;  
+	return timer_read;  
 }
 
 void Timer_delay(long elapse)
 {
-    U32 time;
-    time = Timer_elapsed(0);
-    while(Timer_elapsed(time)<= elapse);
+   U32 start = Timer_Read();
+   U32 now   = 0;
+   while(elapse>0)
+   {
+		now = Timer_Read();
+		if (now > start)
+			elapse-=10;
+		start = now;	
+	 }
 }
